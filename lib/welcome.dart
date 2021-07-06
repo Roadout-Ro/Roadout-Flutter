74a1fa4d1953907e6493347f26746974e2617989
David Retegan <58269698+Dave861@users.noreply.github.com>
2021-07-06 16:44:37 +0300

    Login Buttons

---
+++ lib/welcome.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  WelcomeScreen();
+  WelcomeScreen.forDesignTime();
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          child: Align(
+            child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 270,
+                    height: 60,
+                    child: CupertinoButton.filled(
+                      child: Text(
+                        'Test1',
+                        style: GoogleFonts.karla(
+                            fontSize: 17.0, fontWeight: FontWeight.w600),
+                      ),
+                      onPressed: null,
+                      disabledColor: Color.fromRGBO(255, 193, 25, 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(17.0)),
+                    ),
+                    padding: EdgeInsets.only(bottom: 10.0, right: 20.0),
+                  ),
+                  Container(
+                    width: 298,
+                    height: 70,
+                    child: CupertinoButton.filled(
+                      child: Text(
+                        'Test2',
+                        style: GoogleFonts.karla(
+                            fontSize: 17.0, fontWeight: FontWeight.w600),
+                      ),
+                      onPressed: null,
+                      disabledColor: Color.fromRGBO(143, 102, 13, 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(17.0)),
+                    ),
+                    padding: EdgeInsets.only(bottom: 20.0, right: 20.0),
+                  )
+                ],
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end),
+            alignment: Alignment.bottomRight,
+          ),
+        ),
+      ),
+    );
+  }
+}
