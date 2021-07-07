df9f14a5a203e98c0a39cfdfce4a9d767b951fea
David Retegan <58269698+Dave861@users.noreply.github.com>
2021-07-07 17:39:08 +0300

    -

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -14,48 +14,51 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          constraints: BoxConstraints.expand(),
-          child: Align(
-            child: Column(
-                children: <Widget>[
-                  Container(
-                    width: 270,
-                    height: 60,
-                    child: CupertinoButton.filled(
-                      child: Text(
-                        'Sign In',
-                        style: GoogleFonts.karla(
-                            fontSize: 17.0, fontWeight: FontWeight.w600),
+          child: Column(
+          children: <Widget>[
+            Container(
+                //constraints: BoxConstraints.expand(),
+                child: Align(
+              child: Column(
+                  children: <Widget>[
+
+                    Container(
+                      width: 270,
+                      height: 60,
+                      child: CupertinoButton.filled(
+                        child: Text(
+                          'Sign In',
+                          style: GoogleFonts.karla(
+                              fontSize: 17.0, fontWeight: FontWeight.w600),
+                        ),
+                        onPressed: null,
+                        disabledColor: Color.fromRGBO(255, 193, 25, 1.0),
+                        borderRadius: BorderRadius.all(Radius.circular(17.0)),
                       ),
-                      onPressed: null,
-                      disabledColor: Color.fromRGBO(255, 193, 25, 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(17.0)),
+                      padding: EdgeInsets.only(bottom: 10.0, right: 20.0),
                     ),
-                    padding: EdgeInsets.only(bottom: 10.0, right: 20.0),
-                  ),
-                  Container(
-                    width: 298,
-                    height: 70,
-                    child: CupertinoButton.filled(
-                      child: Text(
-                        'Sign Up',
-                        
-                        style: GoogleFonts.karla(
-                            fontSize: 17.0, fontWeight: FontWeight.w600),
+                    Container(
+                      width: 298,
+                      height: 70,
+                      child: CupertinoButton.filled(
+                        child: Text(
+                          'Sign Up',
+                          style: GoogleFonts.karla(
+                              fontSize: 17.0, fontWeight: FontWeight.w600),
+                        ),
+                        onPressed: null,
+                        disabledColor: Color.fromRGBO(143, 102, 13, 1.0),
+                        borderRadius: BorderRadius.all(Radius.circular(17.0)),
                       ),
-                      onPressed: null,
-                      disabledColor: Color.fromRGBO(143, 102, 13, 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(17.0)),
-                    ),
-                    padding: EdgeInsets.only(bottom: 20.0, right: 20.0),
-                  )
-                ],
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end),
-            alignment: Alignment.bottomRight,
-          ),
+                      padding: EdgeInsets.only(bottom: 20.0, right: 20.0),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end),
+              alignment: Alignment.bottomRight,
+            ))
+          ],
         ),
       ),
     );
