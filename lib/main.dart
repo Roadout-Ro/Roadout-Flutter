b72afcca891df38d4013db9541cd8872add9c1da
David Retegan <58269698+Dave861@users.noreply.github.com>
2021-07-12 15:22:37 +0300

    Bundle ID part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:roadout/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:roadout/utilites.dart';
 
-void main() => runApp(MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MainApp());
+}
 
 class MainApp extends StatelessWidget {
   @override
