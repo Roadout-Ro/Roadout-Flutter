a5e1c5b8ddf134ca3eb398ed95aa61976d25f6bf
David Retegan <58269698+Dave861@users.noreply.github.com>
2021-07-19 19:04:58 +0300

    Auth Persistance

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:roadout/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'mainScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MainApp());
+  if (FirebaseAuth.instance.currentUser?.uid != null) {
+    runApp(MainApp());
+  } else {
+    runApp(WelcomeApp());
+  }
+}
+
+class WelcomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Roadout',
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -15,7 +32,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Roadout',
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      home: MainScreen(),
     );
   }
 }
